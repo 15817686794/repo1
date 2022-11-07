6274e7655ec61cecb7924814d6a95746c74b0c96
adamsu <kwangshien@gmail.com>
2022-11-07 15:40:09 +0800

    adam modify test

--- DummyKey/DummyKey/Frm_Main.Designer.cs
+++ DummyKey/DummyKey/Frm_Main.Designer.cs
@@ -987,7 +987,7 @@
             this.Controls.Add(this.btnESC);
             this.Name = "Frm_Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "20221107江申良修改(2)";
+            this.Text = "20221107AdamTest";
             this.TopMost = true;
             this.ResumeLayout(false);
 
